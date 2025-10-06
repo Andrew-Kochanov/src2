dc49d442813013ceb56b6a3252450c6807fe3297
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-10-06 13:25:53 +0300

    Количество подстрок в строке

--- occur_str.c
+++ occur_str.c
@@ -1,40 +1,54 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+
+char* cross(char* str, int indStart, int indFinish){
+    int len = indFinish - indStart;
+    char *crossStr = (char *)malloc((len + 1) * sizeof(char));
+
+    int indCrStr = 0;
+    int indStr = indStart;
+    while (indStr < indFinish)
+    {
+        crossStr[indCrStr] = str[indStr];
+        indStr++;
+        indCrStr++;
+    }
+
+    crossStr[indCrStr] = '\0';
+
+    return crossStr;
+}
+
 
 int main(int argc, char **argv)
 {
     char str[1000];
+    printf("Введите строку: ");
     fgets(str, sizeof(str), stdin);
-    // char str1[1000];
-    // fgets(str, sizeof(str1), stdin);
-    // int len = strlen(str1);
-    // int count = 0;
-    // for (int i = 0; i < strlen(str); i++)
-    // {
-    //     if (str[i] == '(')
-    //     {
-    //         ratio++;
+    char str1[1000];
+    printf("Введите подстроку: ");
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = '\0';
 
-    //     }
-    //     else if (str[i] == ')')
-    //     {
-    //         ratio -= 1;
-    //     }
-    //     if (ratio < 0)
-    //     {
-    //         printf("Баланса нет");
-    //         return 0;
-    //     }
+    int count = 0;
+    int lenStr = strlen(str);
+    int lenStr1 = strlen(str1);
+    for(int i = 0; i < lenStr - lenStr1 + 1; i++)
+    {
+        char* str2 = cross(str, i, i + lenStr1);
+        if (strcmp(str1, str2) == 0)
+        {
+            printf("%s\n", str2);
+            count++;
+        }
+        free(str2);
+    }
 
-    // }
-    // if (ratio == 0)
-    // {
-    //     printf("Баланс есть");
-    // } else {
-    //     printf("Баланса нет");
-    // }
 
-    strcat(str, 2, 5);
-    printf("%s", str);
+
+
+    printf("Количество подстрок: %d\n", count);
     return 0;
 }
