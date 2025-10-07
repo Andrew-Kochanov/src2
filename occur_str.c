01f694d2407ebbb725264e75d52d7caf1efb0d35
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-10-07 22:06:17 +0300

    Добавил в программы комментарии

--- occur_str.c
+++ occur_str.c
@@ -2,21 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+// функция для создания подстроки из строки по индексам
 char* cross(char* str, int indStart, int indFinish){
-    int len = indFinish - indStart;
-    char *crossStr = (char *)malloc((len + 1) * sizeof(char));
 
-    int indCrStr = 0;
-    int indStr = indStart;
+    // длина подстроки с учетом нулевого элемента
+    int len = indFinish - indStart + 1;
+    // выделение памяти
+    char *crossStr = (char *)malloc((len) * sizeof(char));
+
+    int indCrStr = 0; // индекс для подстроки
+    int indStr = indStart; // индекс для строки
+
     while (indStr < indFinish)
     {
-        crossStr[indCrStr] = str[indStr];
+        crossStr[indCrStr] = str[indStr]; // присвоение элементу подстроки значение элемента строки
         indStr++;
         indCrStr++;
     }
 
-    crossStr[indCrStr] = '\0';
+    crossStr[indCrStr] = '\0'; // добавление нулевого элемента
 
     return crossStr;
 }
@@ -30,24 +34,23 @@ int main(int argc, char **argv)
     char str1[1000];
     printf("Введите подстроку: ");
     fgets(str1, sizeof(str1), stdin);
-    str1[strcspn(str1, "\n")] = '\0';
+    str1[strcspn(str1, "\n")] = '\0'; // меняем знак переноса строки на нулевой
 
     int count = 0;
     int lenStr = strlen(str);
     int lenStr1 = strlen(str1);
+
+    // пробегаем по строке по-элементно, выделяем подстроку, сравнием полученную подстроку с исходной
     for(int i = 0; i < lenStr - lenStr1 + 1; i++)
     {
         char* str2 = cross(str, i, i + lenStr1);
         if (strcmp(str1, str2) == 0)
         {
-            printf("%s\n", str2);
             count++;
         }
+        // освобождаем память
         free(str2);
     }
-
-
-
 
     printf("Количество подстрок: %d\n", count);
     return 0;
