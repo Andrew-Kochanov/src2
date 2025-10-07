1353c29c25e109ddab1eda702ab643b0a88891fc
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-10-07 21:29:12 +0300

    Доработал задачи

---
+++ incom_priv.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int a = 0;
+    int b = 0;
+    printf("Введите число a: ");
+    scanf("%d", &a);
+    printf("Введите число b: ");
+    scanf("%d", &b);
+
+    // счетчик для неполного частного
+    int incomp = 0;
+
+    //коэф прибавляемый к счетчику
+    int count = 1;
+
+    if (a < 0)
+    {
+        a *= -1;
+        count *= -1;
+        incomp -= 1;
+    }
+    if (b < 0)
+    {
+        b *= -1;
+        count *= -1;
+        incomp *= -1;
+    }
+
+    // вычитаем из a число b до тех пор пока b не станет больше а. И прибавляем к счетчику 1.
+    while (a > b)
+    {
+        a = a - b;
+        incomp += count;
+    }
+
+    printf("Неполное частное: %d\n", incomp);
+    return 0;
+}
