a1f08e1eb87cbdc384d9fea10f324e29b59a9653
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-10-08 13:04:23 +0300

    Счастливые билеты через комбинаторику

--- lucky_tick.c
+++ lucky_tick.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-    int sum = 0;
+    int array[28] = {0};
 
-    // 6 циклов для шести цифр и дальнейшее сравнение сумм
-    for (int i_1 = 0; i_1 < 10; i_1++)
+    for (int num = 0; num < 1000; num++)
     {
-        for(int i_2 = 0; i_2 < 10; i_2++)
-        {
-            for(int i_3 = 0; i_3 < 10; i_3++)
-            {
-                for(int i_4 = 0; i_4 < 10; i_4++)
-                {
-                    for (int i_5 = 0; i_5 < 10; i_5++)
-                    {
-                        for (int i_6 = 0; i_6 < 10; i_6++)
-                        {
-                            if(i_1 + i_2 + i_3 == i_4 + i_5 + i_6)
-                            {
-                                sum++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        int sumNumber = (num / 10 % 10) + num / 100 + num % 10;
+        array[sumNumber]++;
     }
 
-    printf("%d\n", sum);
+    int sum = 0;
+    for (int ind = 0; ind < 28; ind++)
+    {
+        sum += pow(array[ind], 2);
+    }
+    printf("Количество счасливых билетов: %d\n", sum);
     return 0;
 }
