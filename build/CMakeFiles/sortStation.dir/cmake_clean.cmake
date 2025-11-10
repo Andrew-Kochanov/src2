5f9a537db05dd3293de4917c5b46f26933de67b6
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-11-10 22:37:40 +0300

    Папка build

---
+++ build/CMakeFiles/sortStation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortStation.dir/link.d"
+  "CMakeFiles/sortStation.dir/sortStation.c.o"
+  "CMakeFiles/sortStation.dir/sortStation.c.o.d"
+  "sortStation"
+  "sortStation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sortStation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
