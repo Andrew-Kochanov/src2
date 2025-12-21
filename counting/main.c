6e6bfb9bf980449fbba1a1ca20ef321c5779b6f0
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-12-21 21:29:22 +0300

    Считалочка

---
+++ counting/main.c
@@ -0,0 +1,44 @@
+#include "../cyclicList/cyclicList.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    // Количество войнов
+    int n = 0;
+    // какого война будут убивать
+    int m = 0;
+
+    while (n <= 0 || m <= 0) {
+        printf("Введите количество войнов: ");
+        scanf("%d", &n);
+        printf("Введите, какого по счету война будут убивать: ")
+            scanf("d", &m);
+    }
+
+    List* warriors = newList();
+    for (int ind = 1; ind < n + 1; ind++) {
+        insert(warriors, ind);
+    }
+
+    int count = 1;
+    while (getSize(warriors) > 1) {
+        // Если дошли до m-го воина
+        if (count == m) {
+            ListNode* toRemove = current;
+            current = current->next;
+            deleteNode(warriors, toRemove->value);
+            // Сбрасываем счетчик
+            count = 1;
+        } else {
+            // Переходим к следующему воину
+            current = current->next;
+            count++;
+        }
+    }
+    printf("Последний выживший был под номером: ");
+    printList(warriors);
+    deleteList(warriors);
+    return 0;
+}
